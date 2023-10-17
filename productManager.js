e6d8d6dcc5c38ee91db8868c0dc85707163e8290
Renzominacore <minacorerenzo@gmail.com>
2023-10-17 13:03:42 -0300

    SegundaEntrega

---
+++ productManager.js
@@ -0,0 +1,89 @@
+const fs=require('fs');
+//const { json } = require('stream/consumers');
+
+class ProductManager{
+    
+    constructor(path){
+        this.productos=[];
+        this.idIncremental=1;
+        this.path=path
+    }
+
+    agregarProductos(title,description,price,thumbnail,code,stock){
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+
+            console.error('Todos los campos son obligatorios.');
+            
+            return
+            
+            }
+        const id=this.idIncremental
+        const producto={
+            id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+        this.idIncremental++
+        const productoExistente = this.productos.find((producto) => producto.code === code);
+
+            if(productoExistente){
+
+        console.log(`El producto: ${producto.code} ya existe`);
+
+        }else{
+            console.log('El producto fue registrado')
+        }
+        this.productos.push(producto);
+        return fs.promises.writeFile(this.path,JSON.stringify(this.productos));
+    }
+    obtenerProductos(){
+        return fs.promises.readFile(this.path)
+        .then((data)=>JSON.parse(data))
+    }
+    async borrarPorId(id) {
+        try {
+          const data = await this.obtenerProductos();
+          const filteredData = data.filter(
+            (producto) => producto.id !== id
+          );
+    
+          if (data.length !== filteredData.length) {
+            await fs.promises.writeFile(this.path, JSON.stringify(data));
+          } else {
+            console.log(`ID ${id} no existe en este archivo`);
+            return null;
+          }
+        } catch (error) {
+          console.log(
+            `Error Code: ${error.code} | Hubo un error (${id})`
+          );
+        }
+      }
+}
+const instancia = new ProductManager('Productos.JSON')
+
+    instancia.agregarProductos(`Estetoscopio`, `Estetoscopio Littman`, 49000, `image`, `cod1`, 7);
+    instancia.agregarProductos(`Oximetro`, `Oximetro Beurer`, 20000, `image`, `cod2`, 10);
+    instancia.agregarProductos(`Tensiometro`, `Tensiometro Philco`, 18000, `image`, `cod3`, 5);
+
+instancia.obtenerProductos().then((productos)=>{
+    console.log(productos)
+})
+instancia.borrarPorId(3)
+
+.then((result) => {
+
+console.log('Resultado:',result);
+
+})
+
+.catch((error) => {
+
+console.error('Error:', error);
+
+});
+
